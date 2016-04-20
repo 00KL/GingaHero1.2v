aa8e739ffc5af2847e59495722b5d014ddf7214b
00KL <lucasmoraesplay@gmail.com>
2016-04-20 10:48:12 -0300

    Pontos
    Agora parece uma janela com a tecla a ser pressione e caso o usuário
    pressione a correta ele ganha um ponto.
    Correções de interface não foram aplicadas para deixar os novos
    elementos adequados visualmente para a página.
    O ID do botão laranja agora é 5.
    A pasta foi realocada

--- public_html/javascript/newjavascript.js
+++ public_html/javascript/newjavascript.js
@@ -3,40 +3,101 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function tecla() {
-	var cor, tecla, caixa;
- 
-	switch (event.keyCode ) {
-		case 97:
-			cor = "green";
-			tecla = "1";
-			document.getElementById("som1").play();
-                        break
-		case 115:
-			cor = "red";
-			tecla = "2";
-			document.getElementById("som2").play();
-                        break
-		case 100:
-			cor = "yellow";
-			tecla = "3";
-			document.getElementById("som3").play();
-                        break
-		case 102:
-			cor = "blue";
-			tecla = "4";
-                        document.getElementById("som4").play();
-			break
-                case 103:
-			cor = "#FF6600";
-			tecla = "6";
-                        document.getElementById("som5").play();
-			break
-	}	
-
-	caixa = document.getElementById(tecla);
-	caixa.style.backgroundColor = "black";
-	setTimeout(function () {caixa.style.backgroundColor = cor;},100);
+var pontos = 1;
+var n = 1;
+window.onload = function () {
+    document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'A' + '</h1>';
+    document.getElementById('9').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + (pontos - 1) + '</h1>';
 }
 
-document.body.onkeypress = tecla;  
+function letra() {
+    
+    mudaN();
+    switch (n) {
+        case 1:
+            document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'A' + '</h1>';
+
+            break
+        case 2:
+            document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'S' + '</h1>';
+
+            break
+        case 3:
+            document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'D' + '</h1>';
+
+            break
+        case 4:
+            document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'F' + '</h1>';
+
+            break
+        case 5:
+            document.getElementById('10').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + 'G' + '</h1>';
+
+            break
+
+            n = Math.floor(Math.random() * 5 + 1);
+    }
+}
+
+
+function pontuacao() {
+    if (pontos > 9) {
+        document.getElementById('9').innerHTML = '<h1 style="margin-left: -28%; margin-top: -31%; color: white">' + (pontos++) + '</h1>';
+
+    } else if (pontos > 99) {
+        document.getElementById('9').innerHTML = '<h1 style="margin-left: -50%; margin-top: -31%; color: white">' + (pontos++) + '</h1>';
+    } else {
+        document.getElementById('9').innerHTML = '<h1 style="margin-left: -6%; margin-top: -31%; color: white">' + (pontos++) + '</h1>';
+
+    }
+}
+
+function tecla() {
+
+    var cor, tecla, caixa;
+    switch (event.keyCode) {
+        case 97:
+            cor = "green";
+            tecla = "1";
+            document.getElementById("som1").play();
+            break
+        case 115:
+            cor = "red";
+            tecla = "2";
+            document.getElementById("som2").play();
+            break
+        case 100:
+            cor = "yellow";
+            tecla = "3";
+            document.getElementById("som3").play();
+            break
+        case 102:
+            cor = "blue";
+            tecla = "4";
+            document.getElementById("som4").play();
+            break
+        case 103:
+            cor = "#FF6600";
+            tecla = "5";
+            document.getElementById("som5").play();
+            break
+    }
+    if (tecla == n) {
+        pontuacao();
+    }
+    
+    letra();
+    
+    caixa = document.getElementById(tecla);
+    caixa.style.backgroundColor = "black";
+    setTimeout(function () {
+        caixa.style.backgroundColor = cor;
+    }, 100);
+
+}
+
+function mudaN() {
+    n = Math.floor(Math.random() * 5 + 1);
+}
+
+document.body.onkeypress = tecla;
